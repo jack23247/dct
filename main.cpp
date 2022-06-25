b93d6499cef35c9fb94b63617833b6bb445b0353
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2022-06-25 19:37:42 +0200

    Add license blurbs, move h_time to submodule

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
+/*
+    Bitmap image compressor using OpenCV's DCT implementation and (optionally)
+    an homegrown algorithm. Provides an A-B comparison functionality and a GUI.
+
+    Copyright (C) 2022  Jacopo Maltagliati
+    Copyright (C) 2022  Alessandro Albi
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+*/
+
 #include <SDL.h>
 #include <SDL_opengl.h>
 
